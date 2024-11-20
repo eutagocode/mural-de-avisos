70374601b9d98c8355c1c3f09ec556d7f2068a9a
eutagocode <thiagoluix21@gmail.com>
2024-11-20 10:14:32 -0300

    feat: :sparkles: Foi feita a importação da biblioteca uuid, e também foi criada a classe Users para usar como modelo de usuario e o array users para salvar os usuários criados. E logo em seguida foram criadas as rotas get e post para ler todos os usuários e criar um novo usuário

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 import express from "express";
+import { v7 } from "uuid";
+const uuid = v7;
 const app = express();
 const PORT = 3000;
+app.use(express.json());
+
+class Users {
+    constructor(name, email) {
+        this.id = uuid();
+        this.name = name;
+        this.email = email;
+    }
+}
+
+const users = [];
+
+app.get("/all", (req, res) => {
+    res.json(JSON.stringify(users));
+});
+
+app.post("/new", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+
+    if (name === "" || email === "") return;
+
+    const user = new Users(name, email);
+
+    users.push(user);
+
+    res.send("Usuário adicionado com sucesso!");
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
