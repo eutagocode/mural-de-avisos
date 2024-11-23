3169335c7c5f3b63e781520ff074ae564d20a527
eutagocode <thiagoluix21@gmail.com>
2024-11-23 16:20:18 -0300

    feat: :sparkles: Foi criada a função de editar um usuário através do formulário que aparece quando clicamos no ícone de editar um usuário, utilizando o método put no fetch, mandando os valores dos inputs do formulário para alterar o nome e email.

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,3 +1,5 @@
+let cardId = "";
+
 const getUsers = async () => {
     const response = await fetch("http://localhost:3000/api/all");
     const data = await response.json();
@@ -7,37 +9,39 @@ const getUsers = async () => {
 
     for (const user of users) {
         const userElement = `
-<div id="${user.id}" class="card">
-    <header class="header_card">
-        <p>${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}</p>
-        <div class="actions_card">
-            <button onclick="editUser(this)">
-                <img
-                    src="./src/assets/images/editsquare.svg"
-                    alt="ícone de lapis"
-                />
-            </button>
-            <button onclick="deleteUser(this)">
-                <img
-                    src="./src/assets/images/delete.svg"
-                    alt="ícone de lixeira"
-                />
-            </button>
-        </div>
-    </header>
-    <main class="main_card">
-        <div class="profile_content">
-            <img
-                src="./src/assets/images/profile.svg"
-                alt="ícone de usuário"
-            />
-        </div>
-        <div class="showcase-text">
-            <h2>${user.name}</h2>
-            <p>${user.email}</p>
-        </div>
-    </main>
-</div>
+            <div id="${user.id}" class="card">
+                <header class="header_card">
+                    <p>${date.getDate()}/${
+            date.getMonth() + 1
+        }/${date.getFullYear()}</p>
+                    <div class="actions_card">
+                        <button onclick="showEditForm(this)">
+                            <img
+                                src="./src/assets/images/editsquare.svg"
+                                alt="ícone de lapis"
+                            />
+                        </button>
+                        <button onclick="deleteUser(this)">
+                            <img
+                                src="./src/assets/images/delete.svg"
+                                alt="ícone de lixeira"
+                            />
+                        </button>
+                    </div>
+                </header>
+                <main class="main_card">
+                    <div class="profile_content">
+                        <img
+                            src="./src/assets/images/profile.svg"
+                            alt="ícone de usuário"
+                        />
+                    </div>
+                    <div class="showcase-text">
+                        <h2>${user.name}</h2>
+                        <p>${user.email}</p>
+                    </div>
+                </main>
+            </div>
         `;
         userElements += userElement;
     }
@@ -76,6 +80,39 @@ const deleteUser = async (element) => {
     getUsers();
 };
 
-const editUser = async (element) => {
-    console.log(element);
+const showEditForm = (element) => {
+    const editContainer = document.getElementById("editContainer");
+
+    if (editContainer.style.display == "none") {
+        editContainer.style.display = "grid";
+    } else {
+        editContainer.style.display = "none";
+        return;
+    }
+
+    const elementId = element.parentElement.parentElement.parentElement.id;
+
+    cardId = elementId;
+};
+
+const editUser = async () => {
+    const cards = document.querySelectorAll(".card");
+    const name = document.getElementById("form-input-name").value;
+    const email = document.getElementById("form-input-email").value;
+
+    const user = { name, email };
+
+    for (let card of cards) {
+        if (card.id == cardId) {
+            await fetch(`http://localhost:3000/api/update/${card.id}`, {
+                method: "put",
+                headers: new Headers({ "content-type": "application/json" }),
+                body: JSON.stringify(user),
+            });
+        }
+    }
+
+    getUsers();
+
+    showEditForm();
 };
